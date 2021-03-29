fd47a7d0eed7db7a403ed1fce5e34bb029ea1d4e
Evgenii-Nazarov <ev66647260@gmail.com>
2021-03-28 22:36:28 -0700

    code clean

--- src/pages/User/types.ts
+++ src/pages/User/types.ts
@@ -7,7 +7,6 @@ export interface IUserAccount {
   createdAt?: string;
   updatedAt?: string;
   transactionTypes?: ITransactionType[];
-  // incomeTypes?: IIncomeType[];
 }
 
 export interface ITransactionType {
